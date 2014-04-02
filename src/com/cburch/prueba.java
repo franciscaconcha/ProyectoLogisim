488ace6fe5ad0342b5e5d3b72b9d394d25c15890
Willy Maikowski <willy.maikowski@gmail.com>
2014-04-02 15:54:41 -0300

    prueba

--- src/com/cburch/prueba.java
+++ src/com/cburch/prueba.java
@@ -2,6 +2,6 @@ package com.cburch;
 
 public class prueba {
 	// cate probando git
-	// nach probando git, al lado de LADY Catherine
-
+	// natxio probando git, al lado de LADY Catherine
+	// mike probando git, al lado derecho de dios padre todo poderoso
 }
