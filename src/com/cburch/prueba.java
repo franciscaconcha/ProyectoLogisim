e220f8ca45d86cbe3cc642df4dcf13528deda22a
cfaundez <cami@cami-R580-R590>
2014-04-02 16:09:04 -0300

    123 probando :D

--- src/com/cburch/prueba.java
+++ src/com/cburch/prueba.java
@@ -4,4 +4,5 @@ public class prueba {
 	// cate probando git
 	// natxio probando git, al lado de LADY Catherine
 	// mike probando git, al lado derecho de dios padre todo poderoso
+	// cami probando git
 }
