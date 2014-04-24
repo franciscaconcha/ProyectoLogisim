2d3fd72d63d4fbf53601bd08950d19aa45f2a5d7
José <jgarridoa@outlook.com>
2014-04-24 12:22:27 -0300

    Implementado Tab Input Archivo en:
    gui/log/InputPanel.java
    
    Agregado a LogFrame.java

---
+++ src/com/cburch/logisim/gui/log/InputPanel.java
@@ -0,0 +1,43 @@
+package com.cburch.logisim.gui.log;
+
+import java.awt.LayoutManager;
+
+/* Nueva tab para ingreso de datos, @José Garrido */
+
+public class InputPanel extends LogPanel {
+
+	public InputPanel(LogFrame frame) {
+		super(frame);
+		// TODO Auto-generated constructor stub
+	}
+
+	public InputPanel(LogFrame frame, LayoutManager manager) {
+		super(frame, manager);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String getTitle() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getHelpText() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void localeChanged() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void modelChanged(Model oldModel, Model newModel) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
