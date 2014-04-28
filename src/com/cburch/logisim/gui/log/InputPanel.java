c8bbd693361707ddf138d2b34164f4183cd38f00
Byron <Byron@Aaron-pc>
2014-04-27 23:08:18 -0400

    Agrege lo basico de la funcionalidad de la pestana. Las dimencioes de la ventana dependenderan del numero de entradas y relojes del circuito (que aun no sabemos como recuperar). Ademas se agrego el boton que permite agregar otra linea de entradas.

--- src/com/cburch/logisim/gui/log/InputPanel.java
+++ src/com/cburch/logisim/gui/log/InputPanel.java
@@ -1,19 +1,80 @@
 package com.cburch.logisim.gui.log;
 
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
 import java.awt.LayoutManager;
+import java.awt.event.ActionEvent;
+import java.util.ArrayList;
+
+import javax.swing.AbstractAction;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextField;
+
 
 /* Nueva tab para ingreso de datos, @José Garrido */
 
 public class InputPanel extends LogPanel {
-
-	public InputPanel(LogFrame frame) {
+   private int numberOfEntries;
+   private ArrayList<JTextField> entradas= new ArrayList<JTextField>();
+   final JPanel panelEntradas = new JPanel();
+   public int getNumberOfEntries(){
+	   return numberOfEntries;
+   }
+   public InputPanel(LogFrame frame){
+	 
+	   this(frame,3,1);
+   }
+	public InputPanel(LogFrame frame,int nEntradas, int relojes) {
 		super(frame);
-		// TODO Auto-generated constructor stub
+		numberOfEntries=  nEntradas+relojes;
+		this.setLayout(new GridLayout(0,1));
+		
+		JPanel titulos = new JPanel();
+		titulos.setLayout(new GridLayout(0, numberOfEntries));
+		for (int i = 0; i < nEntradas ; i++) {
+            titulos.add(new JLabel("Entrada " + String.valueOf(i), JLabel.CENTER));
+        }
+        
+        	for (int i = 0; i < relojes ; i++) {
+                titulos.add(new JLabel("Ticks Reloj " + String.valueOf(i), JLabel.CENTER));
+            }
+        this.add(titulos, BorderLayout.NORTH);
+		final JPanel panelEntradas = new JPanel();
+		panelEntradas.setLayout(new GridLayout(0, numberOfEntries));
+        for (int i = 0; i < numberOfEntries; i++) {
+        	entradas.add(new JTextField());
+            panelEntradas.add(entradas.get(i));
+        }
+        JScrollPane panelEntradasScroll = new JScrollPane(panelEntradas);
+        this.add(panelEntradasScroll,BorderLayout.CENTER);
+        
+        
+        JPanel botones = new JPanel();
+        botones.add(new JButton(new AbstractAction("Agregar Entrada") {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            	
+                
+                for (int i = 0; i < InputPanel.this.getNumberOfEntries(); i++) {
+                JTextField p=new JTextField();
+                entradas.add(p);
+                panelEntradas.add(p);}
+                InputPanel.this.validate();
+            }
+        }));
+   
+        this.add(botones, BorderLayout.SOUTH);
+        
+		
 	}
 
 	public InputPanel(LogFrame frame, LayoutManager manager) {
 		super(frame, manager);
-		// TODO Auto-generated constructor stub
+		
 	}
 
 	@Override
@@ -37,5 +98,9 @@ public class InputPanel extends LogPanel {
 		// TODO Auto-generated method stub
 
 	}
+	
+
+	
+	
 
 }
