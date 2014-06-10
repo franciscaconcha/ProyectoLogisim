a2435cae352f45c51c08beb05737650042ac7a39
José <joseg.njoy@gmail.com>
2014-06-09 20:52:38 -0400

    Validador funcionando
    El validador ahora funciona según la entrada de bits especificadas. Hay
    que encontrar y corregir posibles bugs

--- src/com/cburch/logisim/gui/log/InputPanel.java
+++ src/com/cburch/logisim/gui/log/InputPanel.java
@@ -32,6 +32,8 @@ import com.cburch.logisim.std.wiring.Pin;
 import com.cburch.logisim.util.GraphicsUtil;
 
 import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 class InputPanel extends LogPanel {
 	private int modified=0;
@@ -41,8 +43,9 @@ class InputPanel extends LogPanel {
 	private MyListener myListener = new MyListener();
 	final JPanel entriesPanel = new JPanel();
 	private ArrayList<JTextField> entries= new ArrayList<JTextField>();
-	
-	
+	private ArrayList<Integer> bitWidth= new ArrayList<Integer>();
+
+
 	private class MyListener implements ModelListener {
 		public void selectionChanged(ModelEvent event) {
 			computePreferredSize();
@@ -50,48 +53,48 @@ class InputPanel extends LogPanel {
 		public void entryAdded(ModelEvent event, Value[] values) { 
 			repaint();
 		}
-		
+
 		public void filePropertyChanged(ModelEvent event) { }
 	}
-	
+
 	public InputPanel(LogFrame frame) {
 		super(frame);
 		modelChanged(null, getModel());
 	}
-	
+
 	@Override
 	public String getTitle() {
 		return Strings.get("InputTab");
 	}
-	
+
 	@Override
 	public String getHelpText() {
 		return Strings.get("InputHelp");
 	}
-	
+
 	@Override
 	public void localeChanged() {
 		computePreferredSize();
 		repaint();
 	}
-	
+
 	@Override
 	public void modelChanged(Model oldModel, Model newModel) {
 		if (oldModel != null) oldModel.removeModelListener(myListener);
 		if (newModel != null) newModel.addModelListener(myListener);
 	}	
 
-	
+
 	@Override
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		
+
 		Dimension sz = getSize();
-		
+
 		Model model = getModel();
 		Selection sel = model.getSelection();
 		//if (model == null) return;
-		int columns = selectedIndex.size();
+		final int columns = selectedIndex.size();
 		if (columns == 0) {
 			g.setFont(BODY_FONT);
 			GraphicsUtil.drawCenteredText(g, Strings.get("tableEmptyMessage"), sz.width / 2, sz.height / 2);
@@ -99,94 +102,143 @@ class InputPanel extends LogPanel {
 		}
 		if(modified==0){
 			this.removeAll();
-			this.setLayout(new GridLayout(0,1));
-			JPanel titles = new JPanel();
-			titles.setLayout(new GridLayout(0, columns));		
-
-			for (int i = 0; i < columns; i++) {
-				if(sel.get(i).toString().startsWith("Input")){
-					BitWidth w = sel.get(i).getComponent().getEnd(0).getWidth();
-					int width = w.getWidth();
-					titles.add(new JLabel(sel.get(selectedIndex.get(i)).toShortString()+" ("+Integer.toString(width)+" bits)"), JLabel.CENTER);
-				}
-				else{
-					titles.add(new JLabel(sel.get(selectedIndex.get(i)).toShortString()), JLabel.CENTER);
-				}
-			}
-			this.add(titles, BorderLayout.NORTH);
-			final JPanel entriesPanel = new JPanel();
-			entriesPanel.setLayout(new GridLayout(0, columns));
-			for (int i = 0; i < columns; i++) {
-				entries.add(new JTextField());
-				entriesPanel.add(entries.get(i));
-			}
-			JScrollPane scrollEntriesPanel = new JScrollPane(entriesPanel);
-			this.add(scrollEntriesPanel,BorderLayout.CENTER);
-
-			
 			final JButton submit = new JButton(new AbstractAction(Strings.get("inputSimulate")){
 				public void actionPerformed(ActionEvent e) {
 					System.out.println("apretado");						
 				}
 			});
+			this.setLayout(new GridLayout(0,1));
+			JPanel titles = new JPanel();
+			titles.setLayout(new GridLayout(0, columns));		
+			ArrayList<JLabel> componentes = new ArrayList<JLabel>();
+
+			for (int i = 0; i < columns; i++) {
+				System.out.println(columns + "columns");
+				if(sel.get(i).toString().startsWith(Strings.get("input"))){
+					BitWidth w = sel.get(i).getComponent().getEnd(0).getWidth();
+					int width = w.getWidth();
+					bitWidth.add(width);
+					componentes.add(new JLabel(sel.get(selectedIndex.get(i)).toShortString()+" ("+Integer.toString(width)+" bits)"));
+				}
+				else{
+					componentes.add(new JLabel(sel.get(selectedIndex.get(i)).toShortString()));
+				}
+			}
+			for(int k=0 ; k<componentes.size() ; k++){
+				titles.add(componentes.get(k));
+			}
+			this.add(titles, BorderLayout.NORTH);
+			final JPanel entriesPanel = new JPanel();
+			entriesPanel.setLayout(new GridLayout(0, columns));
+			for (int i = 0; i < columns; i++) {
+				JTextField newTextField=new JTextField();
+				newTextField.getDocument().addDocumentListener(new DocumentListener() {
+					public void changedUpdate(DocumentEvent e) {
+						validateTable();								    
+					}
+					public void removeUpdate(DocumentEvent e) {
+						validateTable();
+					}
+					public void insertUpdate(DocumentEvent e) {
+						validateTable();
+					}
+							
+					public void validateTable(){
+						boolean isInvalid=false;
+						int i=0;
+						for (JTextField entrie : entries) {
+							if(i==columns){ i=0;}
+							int width = bitWidth.get(i);
+							Pattern pattern = Pattern.compile("(1|0){"+width+"}");
+							Matcher mat = pattern.matcher(entrie.getText());
+							if(!mat.matches()){
+								entrie.setForeground(Color.red);
+								isInvalid=true;
+							}
+							else{
+								entrie.setForeground(Color.black);
+							}
+							i++;
+						}
+						if(!isInvalid){
+							submit.setEnabled(true);
+						}
+						else{
+							submit.setEnabled(false);
+							isInvalid=true;
+						}
+						InputPanel.this.validate();	
+					}
+				});
+				entries.add(newTextField);
+				entriesPanel.add(entries.get(i));
+			}
+			JScrollPane scrollEntriesPanel = new JScrollPane(entriesPanel);
+			this.add(scrollEntriesPanel,BorderLayout.CENTER);
+
 
 			JPanel buttons = new JPanel();
 			buttons.add(new JButton(new AbstractAction(Strings.get("inputAddButton")) {
 				@Override
 				public void actionPerformed(ActionEvent e) {
-					
+
 					for (int i = 0; i < selectedIndex.size(); i++) {
 						JTextField newTextField=new JTextField();
 						newTextField.getDocument().addDocumentListener(new DocumentListener() {
-							  public void changedUpdate(DocumentEvent e) {
-							    
-							  }
-							  public void removeUpdate(DocumentEvent e) {
-							    validateTable();
-							  }
-							  public void insertUpdate(DocumentEvent e) {
-								 validateTable();
-							  }
-							  
-							  public void validateTable(){
-								  boolean isValid=false;
-									
-									for (JTextField entrie : entries) {
-										if((!entrie.getText().equals("0")) && (!entrie.getText().equals("1"))){
-											entrie.setForeground(Color.red);
-											isValid=true;
-										}
-										else{
-											entrie.setForeground(Color.black);
-										}
-									}
-									if(!isValid){
-										submit.setEnabled(true);
+							public void changedUpdate(DocumentEvent e) {
+								validateTable();								    
+							}
+							public void removeUpdate(DocumentEvent e) {
+								validateTable();
+							}
+							public void insertUpdate(DocumentEvent e) {
+								validateTable();
+							}
+
+							public void validateTable(){
+								boolean isInvalid=false;
+								int i=0;
+								for (JTextField entrie : entries) {
+									if(i==columns){ i=0;}
+									int width = bitWidth.get(i);
+									Pattern pattern = Pattern.compile("(1|0){"+width+"}");
+									Matcher mat = pattern.matcher(entrie.getText());
+									if(!mat.matches()){
+										entrie.setForeground(Color.red);
+										isInvalid=true;
 									}
 									else{
-										submit.setEnabled(false);
-										isValid=true;
+										entrie.setForeground(Color.black);
 									}
-									InputPanel.this.validate();	
-							  }
-							});
+									i++;
+								}
+								if(!isInvalid){
+									submit.setEnabled(true);
+								}
+								else{
+									submit.setEnabled(false);
+									isInvalid=true;
+								}
+								InputPanel.this.validate();	
+							}
+						});
 						entries.add(newTextField);
 						entriesPanel.add(newTextField);}
 					InputPanel.this.validate();
 				}
 			}));
-			
-			
-			
+
+
+
 			submit.setEnabled(false);
 			buttons.add(submit);
-			
+
 
 			this.add(buttons, BorderLayout.SOUTH);
 			modified=1;
 		}
 	}
-	
+
 	private void computePreferredSize() {
 		Model model = getModel();
 		Selection sel = model.getSelection();
@@ -202,7 +254,7 @@ class InputPanel extends LogPanel {
 			setPreferredSize(new Dimension(0, 0));
 			return;
 		}
-		
+
 		Graphics g = getGraphics();
 		if (g == null) {
 			cellHeight = 16;
@@ -216,7 +268,7 @@ class InputPanel extends LogPanel {
 				cellWidth = Math.max(cellWidth, fm.stringWidth(header));
 			}
 		}
-		
+
 		tableWidth = (cellWidth + COLUMN_SEP) * columns - COLUMN_SEP;
 		tableHeight = cellHeight * + HEADER_SEP;
 		setPreferredSize(new Dimension(tableWidth, tableHeight));*/
