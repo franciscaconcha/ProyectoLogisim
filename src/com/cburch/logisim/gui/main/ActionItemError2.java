846630709b3811493e4ad992ad7cbf21263029bd
nachovalde <ivalderrama.lorca@gmail.com>
2014-06-12 12:58:34 -0400

    Crear nuevo Action listener para el segundo caso del sistema de sugerencias

---
+++ src/com/cburch/logisim/gui/main/ActionItemError2.java
@@ -0,0 +1,14 @@
+package com.cburch.logisim.gui.main;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ActionItemError2 implements ActionListener{
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		System.out.println("prueba funciona");
+		
+	}
+
+}
