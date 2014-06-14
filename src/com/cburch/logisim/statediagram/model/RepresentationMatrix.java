7ff7e3117bbbc4526e7697e091349d768e9eb5fb
raticate <caterina.munoz.v@gmail.com>
2014-06-14 15:40:43 -0400

    Revert "se agrega RepresentationMatrix2"
    This reverts commit 48feda8ddba022ab1b24afab00b75f886b575790.

--- src/com/cburch/logisim/statediagram/model/RepresentationMatrix.java
+++ src/com/cburch/logisim/statediagram/model/RepresentationMatrix.java
@@ -1,4 +1,7 @@
 package com.cburch.logisim.statediagram.model;
+
+import com.cburch.logisim.statediagram.model.exceptions.AbsentStateException;
+import com.cburch.logisim.statediagram.model.exceptions.InvalidTransitionException;
 /**
  * 
  * @author Cate
