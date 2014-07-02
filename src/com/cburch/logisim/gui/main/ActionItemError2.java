a9fa3c3da884f451c07fbbc3525a13e30f92a927
JoseTapiaG <jose.wt@gmail.com>
2014-07-02 14:04:22 -0400

    Se cambia el metodo setAttributeSet privado a publico en AddTool.java. Se agrega listener que agrega un pin con datos en duro

--- src/com/cburch/logisim/gui/main/ActionItemError2.java
+++ src/com/cburch/logisim/gui/main/ActionItemError2.java
@@ -4,8 +4,11 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 
+import com.cburch.logisim.data.AttributeSet;
 import com.cburch.logisim.proj.Project;
+import com.cburch.logisim.std.wiring.Pin;
 import com.cburch.logisim.std.wiring.Wiring;
+import com.cburch.logisim.tools.AddTool;
 import com.cburch.logisim.tools.Tool;
 
 public class ActionItemError2 implements ActionListener {
@@ -20,9 +23,13 @@ public class ActionItemError2 implements ActionListener {
 	public void actionPerformed(ActionEvent e) {
 		Wiring w = new Wiring();
 		Canvas canvas = proj.getFrame().getCanvas();
-		Tool at = w.getTool("Pin");
-		MouseEvent mouseEvent = new MouseEvent(canvas, 0, 0, 0, 10, 10, 0, 0, 0, false, 0); 
-		at.mousePressed(canvas,canvas.getGraphics() , mouseEvent);
+		AddTool at = (AddTool) w.getTool("Pin");
+		AttributeSet atrSet = at.getAttributeSet();
+		atrSet.setValue(Pin.ATTR_TRISTATE, false);
+		at.setAttributeSet(atrSet); 
+		at.setState(canvas, 2);
+		MouseEvent mouseEvent = new MouseEvent(canvas, 0, 0, 0, 170, 44, 50, 50, 1, false, 1); 
+		at.mouseReleased(canvas,canvas.getGraphics() , mouseEvent);
 		canvas.completeAction();
 		System.out.println("prueba funciona");
 	}
