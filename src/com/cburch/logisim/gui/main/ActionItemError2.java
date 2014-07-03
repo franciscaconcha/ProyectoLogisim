6e33b0f185ce96be841bd1d85ed9c09cefa4cefd
JoseTapiaG <jose.wt@gmail.com>
2014-07-03 13:15:55 -0400

    Se agregan wires apretando un boton

--- src/com/cburch/logisim/gui/main/ActionItemError2.java
+++ src/com/cburch/logisim/gui/main/ActionItemError2.java
@@ -4,22 +4,23 @@ import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
-import java.util.Collection;
+import java.util.ArrayList;
 import java.util.Set;
 
-import com.cburch.logisim.circuit.Circuit;
+import com.cburch.logisim.circuit.CircuitAction;
+import com.cburch.logisim.circuit.CircuitMutation;
 import com.cburch.logisim.circuit.CircuitState;
 import com.cburch.logisim.circuit.Wire;
-import com.cburch.logisim.circuit.WireBundle;
-import com.cburch.logisim.comp.Component;
 import com.cburch.logisim.data.AttributeSet;
+import com.cburch.logisim.data.BitWidth;
 import com.cburch.logisim.data.Location;
-import com.cburch.logisim.data.Value;
+import com.cburch.logisim.instance.StdAttr;
+import com.cburch.logisim.proj.Action;
 import com.cburch.logisim.proj.Project;
 import com.cburch.logisim.std.wiring.Pin;
 import com.cburch.logisim.std.wiring.Wiring;
 import com.cburch.logisim.tools.AddTool;
-import com.cburch.logisim.util.GraphicsUtil;
+import com.cburch.logisim.util.StringGetter;
 
 public class ActionItemError2 implements ActionListener {
 
@@ -31,44 +32,55 @@ public class ActionItemError2 implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		
+
 		Set<Wire> wires = proj.getCurrentCircuit().getWires();
 		CircuitState state = proj.getCircuitState();
-		
+		ArrayList<Wire> wiresMutator = new ArrayList<Wire>();
+		Canvas canvas = proj.getFrame().getCanvas();
 		for (Wire wire : wires) {
 			Location s = wire.getE0();
 			Location t = wire.getE1();
 			System.out.println(s);
 			System.out.println(t);
 			System.out.println(state.getValue(s).getColor());
-			if(state.getValue(s).getColor().equals(new Color(192, 0 ,0))){
+			if (state.getValue(s).getColor().equals(new Color(192, 0, 0))) {
 				System.out.println("color correcto");
 				Wiring w = new Wiring();
-				Canvas canvas = proj.getFrame().getCanvas();
+				
 				AddTool at = (AddTool) w.getTool("Pin");
 				AttributeSet atrSet = at.getAttributeSet();
-				//Revisar como alterar el numero de bits de datos
+				// Revisar como alterar el numero de bits de datos
 				atrSet.setValue(Pin.ATTR_TRISTATE, false);
-				at.setAttributeSet(atrSet); 
+				at.setAttributeSet(atrSet);
 				at.setState(canvas, 2);
-				
-				MouseEvent mouseEvent = new MouseEvent(canvas, 0, 0, 0, s.getX()-100, s.getY(), 50, 50, 1, false, 1); 
-				at.mouseReleased(canvas,canvas.getGraphics() , mouseEvent);
+
+				MouseEvent mouseEvent = new MouseEvent(canvas, 0, 0, 0,
+						s.getX() - 100, s.getY(), 50, 50, 1, false, 1);
+				at.mouseReleased(canvas, canvas.getGraphics(), mouseEvent);
 				canvas.completeAction();
-				
-				Location inicio = Location.create(s.getX()-100, s.getY());
-				Location fin = Location.create(s.getX()-50, s.getY());
-				
+
+				Location inicio = Location.create(s.getX() - 100, s.getY());
+				Location fin = Location.create(s.getX() - 50, s.getY());
+
 				Wire newWire = Wire.create(inicio, fin);
-				System.out.println(newWire);
+				
+				wiresMutator.add(newWire);
 				
 			}
-			
+
 		}
-		
-		
-		
-		
+		CircuitMutation mutation = new CircuitMutation(
+				canvas.getCircuit());
+		mutation.addAll(wiresMutator);
+		StringGetter desc;
+		if (wiresMutator.size() == 1)
+			desc = Strings.getter("addWireAction");
+		else
+			desc = Strings.getter("addWiresAction");
+		Action act = mutation.toAction(desc);
+		canvas.getProject().doAction(act);
+		canvas.completeAction();
+
 	}
 
 }
