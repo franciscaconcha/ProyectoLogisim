9df46f7a772259e5c0241682b0eb7d7064b64d81
nachovalde <ivalderrama.lorca@gmail.com>
2014-07-03 10:52:53 -0400

    Tomar los wires

--- src/com/cburch/logisim/gui/main/ActionItemError2.java
+++ src/com/cburch/logisim/gui/main/ActionItemError2.java
@@ -1,16 +1,23 @@
 package com.cburch.logisim.gui.main;
 
+import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
+import java.util.Set;
 
 import com.cburch.logisim.circuit.Circuit;
 import com.cburch.logisim.circuit.CircuitState;
+import com.cburch.logisim.circuit.Wire;
+import com.cburch.logisim.circuit.WireBundle;
 import com.cburch.logisim.data.AttributeSet;
+import com.cburch.logisim.data.Location;
+import com.cburch.logisim.data.Value;
 import com.cburch.logisim.proj.Project;
 import com.cburch.logisim.std.wiring.Pin;
 import com.cburch.logisim.std.wiring.Wiring;
 import com.cburch.logisim.tools.AddTool;
+import com.cburch.logisim.util.GraphicsUtil;
 
 public class ActionItemError2 implements ActionListener {
 
@@ -36,8 +43,16 @@ public class ActionItemError2 implements ActionListener {
 		MouseEvent mouseEvent = new MouseEvent(canvas, 0, 0, 0, 170, 44, 50, 50, 1, false, 1); 
 		at.mouseReleased(canvas,canvas.getGraphics() , mouseEvent);
 		canvas.completeAction();
-		
+		Set<Wire> wires = proj.getCurrentCircuit().getWires();
 		CircuitState state = proj.getCircuitState();
+		
+		for (Wire w2 : wires) {
+			Location s = w2.getE0();
+			Location t = w2.getE1();
+		}
+		
+		
+		
 		
 	}
 
